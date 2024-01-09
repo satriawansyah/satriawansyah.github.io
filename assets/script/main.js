c1887743a9f2c4ef5f8b5ef057fd47056a10370e
Kishiuki <96444529+Kishiuki@users.noreply.github.com>
2024-01-09 17:01:57 +0700

    Burger commit

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,9 +3,20 @@ const navSlide = () => {
   const burger = document.querySelector(".burger");
   const navLists = document.querySelector("nav");
 
+  const closeNavOutsideClick = (event) => {
+    if (!navLists.contains(event.target) && !burger.contains(event.target)) {
+      navLists.classList.remove("nav-active");
+      burger.classList.remove("toggle-burger");
+
+      window.removeEventListener("click", closeNavOutsideClick);
+    }
+  };
+
   burger.addEventListener("click", () => {
     navLists.classList.toggle("nav-active");
     burger.classList.toggle("toggle-burger");
+
+    window.addEventListener("click", closeNavOutsideClick);
   });
 };
 
@@ -22,11 +33,13 @@ window.onbeforeunload = () => {
 function toggleTheme() {
   const body = document.body;
   const themeButton = document.getElementById("themeButton");
+  const burger = document.getElementById("burger");
   const profileImg = document.querySelector(".profile-img");
   const icon = document.getElementById("icon");
 
   body.classList.toggle("dark-mode");
   themeButton.classList.toggle("dark-mode");
+  burger.classList.toggle("dark-mode");
 
   if (body.classList.contains("dark-mode")) {
     profileImg.src = "./assets/images/profile2.png";
